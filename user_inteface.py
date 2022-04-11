5c86403d5b9624e8ab8f5d0e8436a46a1ad258d5
Daniil Kobzar <daniilkobzar@MacBook-Pro-petr.local>
2022-04-12 00:38:06 +0200

    ui module createde with functions, ui created and working, save_contact working

---
+++ user_inteface.py
@@ -0,0 +1,8 @@
+import save_contact as save_con
+
+while True:
+    save_con.write_to_file()
+    break
+
+
+
---
+++ user_interface_functions.py
@@ -0,0 +1,26 @@
+import random
+import csv
+
+def id():
+    with open('PhoneBook.csv', newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        id = len(data) + 1
+        return id
+
+def username():
+    username = input('Please write your name here: ')
+    return username
+
+def surname():
+    surname = input('Please write your surname here: ')
+    return surname
+
+def phone():
+    phone_num = input('Please write your phone number here: ')
+    return phone_num
+
+def comments():
+    comments = input('Please write any comments here (If there are no comments, leave the field blank): ')
+    return comments
+
