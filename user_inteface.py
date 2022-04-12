9024c1cb0309290a06cac4b1d64a4fee5d481205
Yura_Filatov <yrafilatov@gmail.com>
2022-04-12 23:05:52 +0300

    Change user_interface

--- user_inteface.py
+++ user_inteface.py
@@ -1,7 +1,17 @@
 import save_contact as save_con
+import read_contact as read_con
+import export_XML as e_xml
 
 while True:
-    save_con.write_to_file()
+    question = input(f'Выберите действие и нажмите: 1- добавить контакт, 2- найти контакт, 3-изменить контакт, 4 - импортировать тел.книгу')
+    if question==1:
+        save_con.write_to_file()
+    elif question==2:
+        read_con.
+    elif question==3:
+        read_con.  
+    elif question==4:
+        e_xml.export_to_xml()  
     break
 
 
