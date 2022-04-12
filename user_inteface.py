e9bb8a23fcf25f5193c8da5cab9cc9e8653f8a91
Yura_Filatov <yrafilatov@gmail.com>
2022-04-12 23:24:47 +0300

    Change user_interface2

--- user_inteface.py
+++ user_inteface.py
@@ -2,7 +2,7 @@ import save_contact as save_con
 import read_contact as read_con
 import export_XML as e_xml
 
-while True:
+def do_it():
     question = input(f'Выберите действие и нажмите: 1- добавить контакт, 2- найти контакт, 3-посмотреть всю книгу, 4 - импортировать тел.книгу')
     if question==1:
         save_con.write_to_file()
@@ -12,7 +12,10 @@ while True:
         read_con.read_all_book()  
     elif question==4:
         e_xml.export_to_xml()  
-    break
+    else:
+        print('Введено не корректное число')
+        return do_it()
+        
 
 
 
