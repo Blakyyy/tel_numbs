1fc7e87b4c1f60f6f0eb52f41d6643315b47606f
Yura_Filatov <yrafilatov@gmail.com>
2022-04-12 23:24:22 +0300

    Change user_interface

--- user_inteface.py
+++ user_inteface.py
@@ -3,13 +3,13 @@ import read_contact as read_con
 import export_XML as e_xml
 
 while True:
-    question = input(f'Выберите действие и нажмите: 1- добавить контакт, 2- найти контакт, 3-изменить контакт, 4 - импортировать тел.книгу')
+    question = input(f'Выберите действие и нажмите: 1- добавить контакт, 2- найти контакт, 3-посмотреть всю книгу, 4 - импортировать тел.книгу')
     if question==1:
         save_con.write_to_file()
     elif question==2:
-        read_con.
+        read_con.read_exactly(input('Enter surname:').lower())
     elif question==3:
-        read_con.  
+        read_con.read_all_book()  
     elif question==4:
         e_xml.export_to_xml()  
     break
